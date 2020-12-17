8c4fb411f6106afac87aab0ab042424dc89c0dad
cmoe-le <hotcore@posteo.de>
2020-12-17 15:05:11 +0100

    Komplettes Installation-Script hinzugefügt.

---
+++ install-testsetup-from-scratch.sh
@@ -0,0 +1,39 @@
+# Dieses Script führt auf einem frisch installierten Debian 10.7 alle Installationsschritte durch, um die aktuellen Docker-Container für DSpace6 (dspace und dspacedb) aus dem GzFWR-GitHub-Repository zu holen und zu starten. Vorerst zum Erstellen einer DSpace-Testinstanz.
+# Script als Root-User ausführen!
+# Autoren: Jannis, Stefan, Clemens
+# Doku: https://github.com/DSpace/DSpace/tree/dspace-6_x/dspace/src/main/docker-compose
+
+#!/bin/sh
+# Prüfen, ob Script als root ausgeführt wird
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
+# Paketquellen aktualisieren
+apt-get update -y
+apt-get upgrade -y
+
+# Benötigte Pakete installieren
+apt-get install curl -y
+apt-get install git -y
+
+# Docker-Install
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+
+# Docker-Compose "Install"
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Container starten
+cd dspace6-docker
+docker-compose up -d
+echo "5 Minuten warten, bis Container gestartet wurden"
+sleep 5m # Warten bis Container gestartet
+# Administrator-Konto mit Beispiel-Daten anlegen
+docker-compose -p d6 -f docker-compose-cli.yml run dspace-cli create-administrator -e test@test.edu -f admin -l user -p admin -c en
+# Testdaten in Respoitory laden
+docker-compose -p d6 -f docker-compose-cli.yml -f dspace/cli.ingest.yml run dspace-cli
+
+echo "Skript vollständig durchlaufen."
